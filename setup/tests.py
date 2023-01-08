36b73c42bea85d47807f027614e216b1e8cd5880
arthurcezars <arthurcezars@gmail.com>
2023-01-08 19:16:21 -0300

    Primeiro commit

---
+++ setup/tests.py
@@ -0,0 +1,16 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+class AnimaisTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('D:\Repositorio Local\Alura\Programacao\Formação Django\TDD no Django\chromedriver.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_abre_janela_do_chrome(self):
+        self.browser.get(self.live_server_url)
+    
+    def test_deu_ruim(self):
+        """teste de exemplo de erro"""
+        self.fail('Teste falhou - deu ruim mesmo')
