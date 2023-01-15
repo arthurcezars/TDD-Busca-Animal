29492cc01aca622378c711388098c96b9e3c4ad6
arthurcezars <arthurcezars@gmail.com>
2023-01-15 19:07:35 -0300

    Criado um cenário para testar a busca de um determinado animal

--- animais/views.py
+++ animais/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from animais.models import Animal
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'caracteristicas': Animal.objects.all()
+    }
+    return render(request, 'index.html', context)
